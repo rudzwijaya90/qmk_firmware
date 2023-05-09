052800a269d3d98fc73773311cfe66289df1add5
Rudi <rudzwijaya90@gmail.com>
2023-05-10 01:41:56 +0700

    add lets_split & tada68 keymap

---
+++ keyboards/lets_split/keymaps/rudi/rules.mk
@@ -0,0 +1,14 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+LTO_ENABLE      = yes
+TAP_DANCE_ENABLE = yes
+SPLIT_KEYBOARD = yes
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+
+USE_I2C = no
+#to save space
+MIDI_ENABLE = no            # MIDI controls
+AUDIO_ENABLE = no           # Audio output on port C6
+MUSIC_ENABLE = no 
+SPACE_CADET_ENABLE = no
+#to save space
